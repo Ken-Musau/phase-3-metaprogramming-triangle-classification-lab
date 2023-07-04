3c3121f6abff3100355f21fa1a809a6511e3c79a
Kennedy Musau <kennedy.musauu@gmail.com>
2023-07-05 01:14:26 +0300

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 =side3
+  end
+
+  def kind
+    sides_positive = side1 > 0 && side2 > 0 && side3 > 0
+    triangle_inequality = (side1 + side2 > side3) && (side2 + side3 > side1) && (side3 + side1 > side2)
+    if (sides_positive && triangle_inequality)
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "This is an invalid triangle"
+    end
+  end
+
 end
+
+shape = Triangle.new(2,2,2)
+
+puts shape.kind
